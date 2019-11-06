23da3a9e89ed3762259b35c989c4cd4bb3fc124c
Ilonis <masteroforigami@hotmail.com>
2019-11-06 15:50:58 -0600

    1 file modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, FlexLayoutModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, BodyComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
