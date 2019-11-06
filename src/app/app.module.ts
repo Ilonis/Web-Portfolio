53055e3a457d60193379ec1b250382c14570763c
Ilonis <masteroforigami@hotmail.com>
2019-11-06 16:37:11 -0600

    Figured out Routing/Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
@@ -8,10 +9,33 @@ import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
+import { CardComponent } from './card/card.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, FlexLayoutModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, BodyComponent, FooterComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+    ]),
+    FlexLayoutModule 
+  ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent, 
+    HeaderComponent, 
+    BodyComponent, 
+    FooterComponent, 
+    CardComponent, 
+    AboutComponent, HomeComponent 
+  ],
+  bootstrap:    [ 
+    HeaderComponent, 
+    BodyComponent, 
+    FooterComponent 
+  ]
 })
 export class AppModule { }
