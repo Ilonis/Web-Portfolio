0bdd5375e478fcd474d8e752595e61fcb5c96ba9
Ilonis <57330806+Ilonis@users.noreply.github.com>
2019-11-07 01:25:12 -0600

    Update projects.ts

---
+++ src/app/projects.ts
@@ -0,0 +1,30 @@
+export const projects = [
+  {
+    name: 'Evolve',
+    url: 'https://www.evolvetemplate.com/',
+    image: 'https://github.com/Ilonis/Web-Portfolio/blob/Initial-Build/src/app/imgs/evolve_main.jpg?raw=true',
+    shortDescrip: 'Responsive, interactive, hero slider.',
+    description: 'Created from Designer\'s mockup. Functionally built from scratch.  Features Color Swapper (Top Left). This feature allows prospective clients or salespeople to showcase the different colors for the theme on the fly.'
+  },
+  {
+    name: 'Luna',
+    url: 'https://www.lunalightins.com/',
+    image: 'https://github.com/Ilonis/Web-Portfolio/blob/Initial-Build/src/app/imgs/luna_main.jpg?raw=true',
+    shortDescrip: 'Responsive, interactive, hero image.',
+    description: 'Created from Designer\'s mockup. Functionally built from scratch.  Features Color Swapper (Top Left). This feature allows prospective clients or salespeople to showcase the different colors for the theme on the fly.'
+  },
+  {
+    name: 'Evolve',
+    url: 'https://www.evolvetemplate.com/',
+    image: 'https://github.com/Ilonis/Web-Portfolio/blob/Initial-Build/src/app/imgs/evolve_main.jpg?raw=true',
+    shortDescrip: 'Responsive, interactive, hero slider.',
+    description: 'Created from Designer\'s mockup. Functionally built from scratch.  Features Color Swapper (Top Left). This feature allows prospective clients or salespeople to showcase the different colors for the theme on the fly.'
+  },
+  {
+    name: 'Evolve',
+    url: 'https://www.evolvetemplate.com/',
+    image: 'https://github.com/Ilonis/Web-Portfolio/blob/Initial-Build/src/app/imgs/evolve_main.jpg?raw=true',
+    shortDescrip: 'Responsive, interactive, hero slider.',
+    description: 'Created from Designer\'s mockup. Functionally built from scratch.  Features Color Swapper (Top Left). This feature allows prospective clients or salespeople to showcase the different colors for the theme on the fly.'
+  }
+];
