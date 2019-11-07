b416a72e5380477a1baf9d85a0c15549d651dd46
Ilonis <masteroforigami@hotmail.com>
2019-11-07 01:28:45 -0600

    2 files modified

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { projects } from '../projects';
+
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
+  projects = projects;
 
   constructor() { }
 
