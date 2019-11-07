48de6a1f7262728cd820b8c1aa8b6c16b0c5a076
Ilonis <masteroforigami@hotmail.com>
2019-11-07 01:51:02 -0600

    Card Functions

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  overlayActive: boolean;
+
+  constructor() {
+      this.overlayActive = false;
+  }
 
   ngOnInit() {
   }
