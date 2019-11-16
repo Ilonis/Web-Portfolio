32e182a7c9610bb8cf716797e37f016de6d47732
Ilonis <masteroforigami@hotmail.com>
2019-11-16 17:17:01 -0600

    1 file modified

--- src/app/project-details.ts
+++ src/app/project-details.ts
@@ -73,11 +73,44 @@ export const projectDetails = [
   {
     index: 2,
     image: 'https://github.com/Ilonis/Web-Portfolio/blob/Initial-Build/src/app/imgs/hazel_main.jpg?raw=true',
+    width: 1000,
+    height: 2852,
     descripts: [
-      'Test', 
-      'Another',
-      'One More',
-      'Last',
+      {
+        coordx: 50,
+        coordy: 50,
+        words:'Test', 
+      },
+      {
+        coordx: 200,
+        coordy: 400,
+        words:'Test2', 
+      },
+      {
+        coordx: 700,
+        coordy: 250,
+        words:'Test3', 
+      },
+      {
+        coordx: 800,
+        coordy: 800,
+        words:'Test4', 
+      },
+      {
+        coordx: 250,
+        coordy: 1100,
+        words:'Test5', 
+      },
+      {
+        coordx: 500,
+        coordy: 1500,
+        words:'Test6', 
+      },
+      {
+        coordx: 400,
+        coordy: 2000,
+        words:'Test7', 
+      },
     ],
   },
   {
